b0ed44105e64676bcc961d002100ff7b36236620
Sergey Zhurbenko <sergey.zhurbenko@solomoto.com>
2016-05-20 20:30:31 +0300

    First usable sample

---
+++ GetJiraComments/GetJiraComments.py
@@ -0,0 +1,34 @@
+# This script shows how to use the client in anonymous mode
+# against jira.atlassian.com.
+import re
+import urllib3.contrib.pyopenssl
+urllib3.contrib.pyopenssl.inject_into_urllib3()
+from jira import JIRA
+
+
+# By default, the client will connect to a JIRA instance started from the Atlassian Plugin SDK
+# (see https://developer.atlassian.com/display/DOCS/Installing+the+Atlassian+Plugin+SDK for details).
+# Override this with the options parameter.
+jira_options = {
+    'server': 'https://solomoto.atlassian.net'}
+jira = JIRA(options=jira_options, basic_auth=('sergey.zhurbenko', 'Asdqzec2012!?'))
+
+# Get all projects viewable by anonymous users.
+projects = jira.projects()
+
+# Sort available project keys, then return the second, third, and fourth keys.
+keys = sorted([project.key for project in projects])[2:5]
+
+# Get an issue.
+issue = jira.issue('SOLO-764')
+print issue.fields.project.key             # 'JRA'
+print issue.fields.issuetype.name          # 'New Feature'
+print issue.fields.reporter.displayName    # 'Mike Cannon-Brookes [Atlassian]'
+print issue.fields.comment.comments
+
+# Find all comments made by solomoto on this issue.
+solo_comments = [comment for comment in issue.fields.comment.comments
+                if re.search(r'@solomoto.com$', comment.author.emailAddress)]
+
+# Add a comment to the issue.
+#jira.add_comment(issue, 'Comment text')
